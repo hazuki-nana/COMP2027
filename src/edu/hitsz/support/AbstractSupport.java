cfe95c4c8da0b6aaaae7fcd900ae937112bad697
hazuki <2468892775@qq.com>
2024-04-08 19:57:50 +0800

    第二次实验v3

--- src/edu/hitsz/support/AbstractSupport.java
+++ src/edu/hitsz/support/AbstractSupport.java
@@ -9,14 +9,7 @@ public abstract class AbstractSupport extends AbstractFlyingObject {
         super(locationX, locationY, speedX, speedY);
     }
 
-    @Override
-    public void forward() {
-        super.forward();
-        // 判定 y 轴向下飞行出界
-        if ( locationX<=0 || locationX>=Main.WINDOW_WIDTH || locationY<=0 || locationY>=Main.WINDOW_HEIGHT){
-            this.vanish();
-        }
-    }
+
 
     public abstract void Effect(HeroAircraft heroAircraft);
 
